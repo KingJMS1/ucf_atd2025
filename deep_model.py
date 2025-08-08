7fc852381ec002a8a147c9348b81c5d28e11909e
KingJMS1 <1kingjms@gmail.com>
2025-08-08 20:45:12 +0000

    Update deep model

--- deep_model.py
+++ deep_model.py
@@ -4,6 +4,9 @@ import torch.nn as nn
 from time import time
 import pyarrow.parquet as pq
 import random
+import itertools as it
+import gc
+import tqdm
 
 from ucf_atd_model.data import data_loc, ResultCache
 
@@ -18,45 +21,64 @@ device = pt.device("cuda:0")
 model = nn.Sequential(
     nn.Linear(inp_dim, h_dim),
     nn.ReLU(),
-    nn.Dropout(0.2),
+    nn.Dropout(0.3),
     nn.Linear(h_dim, h_dim),
     nn.ReLU(),
-    nn.Dropout(0.2),
+    nn.Dropout(0.3),
     nn.Linear(h_dim, h_dim // 2),
     nn.ReLU(),
-    nn.Dropout(0.2),
+    nn.Dropout(0.3),
     nn.Linear(h_dim // 2, h_dim // 10),
     nn.ReLU(),
+    nn.Dropout(0.1),
     nn.Linear(h_dim // 10, 1),
     nn.Sigmoid()
 ).double().to(device)
 
+xmean = pt.load("xmean.pt")
+xstd = pt.load("xstd.pt")
 
-table: pd.DataFrame = None
-validation: pd.DataFrame = None
-with pq.ParquetFile(data_loc("link_data_small.parquet")) as fulldata:
-    n_rowgroups = fulldata.num_row_groups
-    validation = fulldata.read_row_group(0).to_pandas()
-    table = fulldata.read_row_groups(random.sample(list(range(1, fulldata.num_row_groups)), 1200)).to_pandas(self_destruct = True)
+num_loops = None
+validation = None
+def data_generator():
+    global validation
+    global xstd
+    global xmean
+    global num_loops
 
-labels = table.keys()
-train_X = pt.from_numpy(table.drop(["label"], axis=1).values)
-xmean = pt.mean(train_X, 0)
-xstd = pt.std(train_X, 0)
-train_X = (train_X - xmean) / xstd
-train_y = pt.from_numpy(table["label"].to_numpy()).double()
+    batch_size = 1
+    table: pd.DataFrame = None
+    with pq.ParquetFile(data_loc("link_data_small.parquet")) as fulldata:
+        n_rowgroups = fulldata.num_row_groups
+        all_data = set(range(n_rowgroups))
+        if validation is None:
+            validation = random.sample(sorted(all_data), 3)
+        all_data = all_data.difference(validation)
+        testData = fulldata.read_row_groups(validation).to_pandas()
+        
+        test_X = pt.from_numpy(testData.drop(["label"], axis=1).values)
+        test_X = (test_X - xmean) / xstd
+        test_X = test_X.to(device)
+        test_y = pt.from_numpy(testData["label"].to_numpy()).double().to(device)
+        
+        all_data = sorted(list(all_data))
+        num_loops = len(list(it.batched(all_data, batch_size)))
 
-test_X = pt.from_numpy(validation.drop(["label"], axis=1).values)
-test_X = (test_X - xmean) / xstd
-test_X = test_X.to(device)
-test_y = pt.from_numpy(validation["label"].to_numpy()).double().to(device)
+        yield test_X, test_y
 
-pt.save(xmean, "xmean.pt")
-pt.save(xstd, "xstd.pt")
+        for idxs in it.batched(all_data, batch_size):
+            table = fulldata.read_row_groups(idxs).to_pandas(self_destruct = True)
+            
+            train_X = pt.from_numpy(table.drop(["label"], axis=1).values)
+            xmean = pt.mean(train_X, 0)
+            xstd = pt.std(train_X, 0)
+            train_X = (train_X - xmean) / xstd
+            train_y = pt.from_numpy(table["label"].to_numpy()).double()
+            
+            gc.collect()
 
-train_dataset = pt.utils.data.TensorDataset(train_X, train_y)
+            yield train_X, train_y
 
-train_dataloader = pt.utils.data.DataLoader(train_dataset, batch_size=8192, num_workers=10, shuffle=True, pin_memory=True)
 
 num_epochs = 100
 
@@ -66,33 +88,37 @@ lossfn = nn.BCELoss()
 for epoch in range(num_epochs):
     print(f"Epoch {epoch} / {num_epochs}")
     startTime = time()
-    # Train model
-    model.train()
+    
     epoch_train_loss = 0
-    for input, label in train_dataloader:
-        input = input.to(device)
-        label = label.to(device)
+
+    dataloader = data_generator()
+    X_test, y_test = next(dataloader)
+
+    # Evaluate model
+    model.eval()
+    with pt.no_grad():
+        output = model(X_test)
+        loss = lossfn(output.flatten(), y_test)
+        print(f"    Test Loss: {loss.item():.3f}")
+
+    # Train the model
+    model.train()
+    for X_train, y_train in tqdm.tqdm(dataloader, total=num_loops):
+        X_train = X_train.to(device)
+        y_train = y_train.to(device)
         optimizer.zero_grad()
         
         output = None
         loss = None
         with pt.set_grad_enabled(True):
-            output = model(input)
-            loss = lossfn(output.flatten(), label)
+            output = model(X_train)
+            loss = lossfn(output.flatten(), y_train)
             loss.backward()
             optimizer.step()
         
         epoch_train_loss += loss.item()
     
     print(f"    Train Loss: {epoch_train_loss:.3f}")
-    
-    model.eval()
-    # Evaluate model
-    with pt.no_grad():
-        output = model(input)
-        loss = lossfn(output.flatten(), label)
-        print(f"    Test Loss: {loss.item():.3f}")
-    
     endTime = time()
     print(f"    Time Elapsed: {(endTime - startTime):.3f} seconds")
 
