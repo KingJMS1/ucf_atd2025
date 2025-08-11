cfa2b45d50505c35898b53a70b156e067e7d23d3
Mahlon Scott <1kingjms@gmail.com>
2025-08-11 13:49:55 -0400

    Iter

--- dist_deep20.sh
+++ dist_deep20.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=deep20
 #SBATCH --output=deep20_%j.txt    
-#SBATCH --nodes=8                          # number of nodes you want to use
-#SBATCH --ntasks=8                        # number of processes to be run
+#SBATCH --nodes=12                          # number of nodes you want to use
+#SBATCH --ntasks=12                        # number of processes to be run
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 #SBATCH --gpus-per-node=1                 # every node wants one GPU
 #SBATCH --constraint=h100                 # get h100s
 #SBATCH --gpu-bind=none                   # NCCL can't deal with task-binding...
-#SBATCH --time=12:00:00
+#SBATCH --time=4:00:00
 
 module load anaconda
 conda activate pyproj
