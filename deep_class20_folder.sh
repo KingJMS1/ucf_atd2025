fb872ce3b9b1bfbd87eed9760a34aa3992eecb3d
Mahlon Scott <1kingjms@gmail.com>
2025-08-13 21:38:10 -0400

    Iter

--- deep_class20_folder.sh
+++ deep_class20_folder.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=small_deep20
 #SBATCH --output=small_deep20_%j.txt    
 #SBATCH --ntasks=1                        # number of processes to be run
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 #SBATCH --gpus-per-node=1                 # every node wants one GPU
 #SBATCH --constraint=h100                 # get h100s
-#SBATCH --time=14:00:00
+#SBATCH --time=7:00:00
 
 module load anaconda
 conda activate pyproj
