7da71118b0dfffaf41bc92f488a06c1205c34d76
KingJMS1 <1kingjms@gmail.com>
2025-09-28 01:27:18 +0000

    Start cleaning up for book export

--- run_no_acc.py
+++
@@ -1,8 +0,0 @@
-from ucf_atd_model import river_and_base_model, baseline, data
-import atd2025
-
-file = "dataset3.csv"
-result, path = river_and_base_model.run(file)
-# resultB, pathB = baseline.run(file)
-
-print(path)
